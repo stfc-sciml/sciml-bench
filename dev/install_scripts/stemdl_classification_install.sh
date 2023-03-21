354a92f9ab5af4a7481d56a6a54866c8e86a8655
Samuel Jackson <samueljackson@outlook.com>
2023-03-21 16:49:09 +0000

    Update install scripts

--- dev/install_scripts/stemdl_classification_install.sh
+++ dev/install_scripts/stemdl_classification_install.sh
@@ -6,7 +6,7 @@ set -x
 ENV_NAME=sciml-bench-stemdl_classification
 conda remove -n $ENV_NAME --all -y --quiet
 conda create -n $ENV_NAME python=3.9 -y --quiet
-ENV_PATH=$(dirname $(dirname /home/lhs18285/miniconda3/bin/conda))/envs/$ENV_NAME
+ENV_PATH=$(dirname $(dirname $(which conda)))/envs/$ENV_NAME
 
 # Install conda requirements
 conda install -n $ENV_NAME -y --quiet pytorch==1.13.1 torchvision==0.14.1 pytorch-cuda=11.6 -c pytorch -c nvidia
