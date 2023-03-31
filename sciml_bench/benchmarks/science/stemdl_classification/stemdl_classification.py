adc75ba970fe185d2075f34adf063f8db86265d7
Samuel Jackson <samueljackson@outlook.com>
2023-03-31 06:50:16 +0000

    Update stemdl benchmark with metrics

--- sciml_bench/benchmarks/science/stemdl_classification/stemdl_classification.py
+++ sciml_bench/benchmarks/science/stemdl_classification/stemdl_classification.py
@@ -14,6 +14,7 @@
 # P.O. Box 2008, Oak Ridge, TN 37831 USA. 
 # All rights reserved.
 
+import yaml
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -46,6 +47,7 @@ from pathlib import Path
 from torch.utils.data import Dataset
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import torchmetrics as tm
 
 # Integration into sciml-bench framework
 from sciml_bench.core.runtime import RuntimeIn, RuntimeOut
@@ -69,7 +71,7 @@ class NPZDataset(Dataset):
         return len(self.files)
 
 # LitAutoEncoder
-class LitAutoEncoder(pl.LightningModule):
+class StemDLClassifier(pl.LightningModule):
     def __init__(self):
         super().__init__()
         self.input_size = 128
@@ -79,6 +81,9 @@ class LitAutoEncoder(pl.LightningModule):
         self.num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(self.num_ftrs, self.num_classes)
         self.feature_extract = False
+        self.accuracy = tm.classification.MulticlassAccuracy(self.num_classes)
+        self.accuracy = tm.classification.MulticlassAccuracy(self.num_classes)
+        self.f1_score = tm.classification.F1Score(task='multiclass', num_classes=self.num_classes, average='macro')
 
     # forward step
     def forward(self, x):
@@ -86,33 +91,48 @@ class LitAutoEncoder(pl.LightningModule):
         return embedding
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=0.001, weight_decay=0.00005)
         return optimizer
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
         x_hat = self.model(x)
-        y = F.one_hot(y, num_classes=231).float()
-        loss = F.mse_loss(x_hat, y)
+        loss = F.cross_entropy(x_hat, y)
         self.log('train_loss', loss)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
-        x_hat = self.model(x)
-        y = F.one_hot(y, num_classes=231).float()
-        loss = F.mse_loss(x_hat, y)
+        logits = self.model(x)
+        loss = F.cross_entropy(logits, y)
         self.log('val_loss', loss)
+        y_hat = F.softmax(logits, dim=-1)
+        self.accuracy(y_hat, y)
+        self.f1_score(y_hat, y)
         return loss
+
+    def validation_epoch_end(self, outputs):
+        avg_accuracy = self.accuracy.compute()
+        avg_f1 = self.f1_score.compute()
+        self.log('valid_accuracy', avg_accuracy, prog_bar=True)
+        self.log('valid_accuracy', avg_f1, prog_bar=True)
 
     def test_step(self, test_batch, batch_idx):
         x, y = test_batch
-        x_hat = self.model(x)
-        y = F.one_hot(y, num_classes=231).float()
-        loss = F.mse_loss(x_hat, y)
-        self.log('test_loss', loss)
-        return loss 
-    
+        logits = self.model(x)
+        y_hat = F.softmax(logits, dim=-1)
+        loss = F.cross_entropy(logits, y)
+        self.log('loss', loss)
+        self.accuracy(y_hat, y)
+        self.f1_score(y_hat, y)
+
+    def test_epoch_end(self, outputs):
+        avg_accuracy = self.accuracy.compute()
+        avg_f1 = self.f1_score.compute()
+
+        self.log('accuracy', avg_accuracy * 100)
+        self.log('f1', avg_f1 * 100)
+
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         x, y = batch
         y_hat = self.model(x)
@@ -129,7 +149,7 @@ def sciml_bench_training(params_in: RuntimeIn, params_out: RuntimeOut):
     # Entry point for the training routine to be called by SciML-Bench
     default_args = {
         'batchsize': 32,
-        'epochs': 5,
+        'epochs': 10,
         'nodes': 1,
         'gpus': 1
     }    
@@ -177,7 +197,7 @@ def sciml_bench_training(params_in: RuntimeIn, params_out: RuntimeOut):
 
     # Model
     with log.subproc('Create data model'):
-        model = LitAutoEncoder()
+        model = StemDLClassifier()
 
     # Training
     with log.subproc('Start training'):
@@ -187,7 +207,8 @@ def sciml_bench_training(params_in: RuntimeIn, params_out: RuntimeOut):
 
     # Testing
     with log.subproc('Start testing'):
-        trainer.test(model, test_loader)
+        metrics = trainer.test(model, test_loader)
+        metrics = metrics[0]
         log.message('End testing')
 
     # Save model
@@ -195,6 +216,13 @@ def sciml_bench_training(params_in: RuntimeIn, params_out: RuntimeOut):
     with log.subproc('Save model'):
         torch.save(model.state_dict(), model_path)
         log.message('Model saved')
+
+    # Save metrics
+    metrics = {key: float(value) for key, value in metrics.items()}
+    metrics_file = params_in.output_dir / 'metrics.yml'
+    with log.subproc('Saving inference metrics to a file'):
+        with open(metrics_file, 'w') as handle:
+            yaml.dump(metrics, handle)  
 
      # End top level
     log.ended(f'Running benchmark stemdl_classification on training mode')
@@ -248,14 +276,21 @@ def sciml_bench_inference(params_in: RuntimeIn, params_out: RuntimeOut):
 
     # Load model
     with log.subproc('Load model'):
-        model = LitAutoEncoder()
+        model = StemDLClassifier()
         model.load_state_dict(torch.load(model_path))
 
     # Start inference
     with log.subproc('Inference on the model'):
         trainer = pl.Trainer(gpus=gpus, num_nodes=nodes, precision=16, strategy="ddp", default_root_dir=params_in.output_dir)
-        trainer.predict(model, dataloaders=predict_loader)
+        metrics = trainer.test(model, dataloaders=predict_loader)
+        metrics = metrics[0]
+
+    # Save metrics
+    metrics = {key: float(value) for key, value in metrics.items()}
+    metrics_file = params_in.output_dir / 'metrics.yml'
+    with log.subproc('Saving inference metrics to a file'):
+        with open(metrics_file, 'w') as handle:
+            yaml.dump(metrics, handle)  
 
     # End top level
     log.ended('Running benchmark stemdl_classification on inference mode')
-###
