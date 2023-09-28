65b472fcda90494d8a558435e6b524488d84a62f
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2023-09-28 13:19:52 +0000

    Add pytorch benchmark for mnist

--- sciml_bench/benchmarks/examples/mnist_torch/mnist_torch.py
+++ sciml_bench/benchmarks/examples/mnist_torch/mnist_torch.py
@@ -6,7 +6,7 @@
 # SciML-Bench
 # Copyright © 2021 Scientific Machine Learning Research Group
 # Scientific Computing Department, Rutherford Appleton Laboratory
-# Science and Technology Facilities Council, UK. 
+# Science and Technology Facilities Council, UK.
 # All rights reserved.
 
 """
@@ -19,49 +19,59 @@ This is a single device training/inference example.
 from sciml_bench.core.runtime import RuntimeIn, RuntimeOut
 
 # libs required by implementation
-from pathlib import Path
 import yaml
 import torch
-import torch.utils.data as Data
-import torch.multiprocessing as mp
-import torch.nn as nn
+import torchmetrics as tm
 import torch.nn.functional as F
 import h5py
-import numpy as np
+import wandb
 from torch.utils.data import DataLoader
 
 # implementation from other files
-from sciml_bench.benchmarks.examples.mnist_torch.mnist_torch_misc \
-    import MNISTDataset, MNISTNet
-
+from sciml_bench.benchmarks.examples.mnist_torch.mnist_torch_misc import (
+    MNISTDataset,
+    MNISTNet,
+)
 
 
 def create_model_mnist():
-    model = MNISTNet(32, 3, 128)
+    model = MNISTNet(32, 128)
     return model
-    
-
-def load_dataset_mnist(file_path, batch_size):
-    """ mnist dataset """
-    with h5py.File(file_path, 'r') as h5:
-        images = torch.tensor(h5['image'][:], dtype=torch.float32)
-        labels = torch.tensor(h5['label'][:], dtype=torch.long)
-        # expand channel dim and normalize
-        images = torch.unsqueeze(images, 1) / 255
 
 
 def train(model, train_loader, optimizer):
     model.train()
-    train_losses  = []
+    accuracy_metric = tm.Accuracy("multiclass", num_classes=10)
+    loss_metric = tm.MeanMetric()
+
     for _, (data, target) in enumerate(train_loader):
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+
+        loss = F.cross_entropy(output, target)
         loss.backward()
         optimizer.step()
-        train_losses.append(loss.item())
 
-    return train_losses
+        accuracy_metric(output, target)
+        loss_metric(loss)
+
+    wandb.log({"loss": loss_metric.compute(), "accuracy": accuracy_metric.compute()})
+
+
+def predict(model, test_loader):
+    model.eval()
+    accuracy_metric = tm.Accuracy("multiclass", num_classes=10)
+    loss_metric = tm.MeanMetric()
+
+    for _, (data, target) in enumerate(test_loader):
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        accuracy_metric(output, target)
+        loss_metric(loss)
+
+    wandb.log(
+        {"val_loss": loss_metric.compute(), "val_accuracy": accuracy_metric.compute()}
+    )
 
 
 def sciml_bench_training(params_in: RuntimeIn, params_out: RuntimeOut):
@@ -69,77 +79,82 @@ def sciml_bench_training(params_in: RuntimeIn, params_out: RuntimeOut):
     Main entry of `sciml_bench run` for a benchmark instance
     in the training mode. Please consult the API documentation.
     """
-    params_out.activate(rank=0, local_rank=0,
-                      activate_log_on_host=False,
-                      activate_log_on_device=True, console_on_screen=True)
+    suggested_args = {"batch_size": 128, "epochs": 2}
+    args = params_in.bench_args.try_get_dict(default_args=suggested_args)
+
+    wandb.init(project="mnist_torch", config=args)
+
+    params_out.activate(
+        rank=0,
+        local_rank=0,
+        activate_log_on_host=False,
+        activate_log_on_device=True,
+        console_on_screen=True,
+    )
 
     log = params_out.log
-    log.begin('Running benchmark mnist_torch on training mode')
+    log.begin("Running benchmark mnist_torch on training mode")
 
     # top-level process
     console = params_out.log.console
-    console.begin('Running benchmark mnist_torch on training mode')
+    console.begin("Running benchmark mnist_torch on training mode")
 
-    # We expect two benchmark-specific arguments here: 
-    # batch_size and epochs. If not, we will assign 
+    # We expect two benchmark-specific arguments here:
+    # batch_size and epochs. If not, we will assign
     # default values.
-    with log.subproc('Parsing input arguments'):
+    with log.subproc("Parsing input arguments"):
         # hyperparameters
-        suggested_args = {
-            'batch_size': 128,
-            'epochs': 2
-        } 
+        suggested_args = {"batch_size": 128, "epochs": 2}
         args = params_in.bench_args.try_get_dict(default_args=suggested_args)
-        batch_size = args['batch_size']
-        epochs = args['epochs']
-        log.message(f'batch_size = {batch_size}')
-        log.message(f'epochs     = {epochs}')
+        batch_size = args["batch_size"]
+        epochs = args["epochs"]
+        log.message(f"batch_size = {batch_size}")
+        log.message(f"epochs     = {epochs}")
 
-    with log.subproc('Writing the argument file'):
-        args_file = params_in.output_dir / 'arguments_used.yml'
-        with open(args_file, 'w') as handle:
+    with log.subproc("Writing the argument file"):
+        args_file = params_in.output_dir / "arguments_used.yml"
+        with open(args_file, "w") as handle:
             yaml.dump(args, handle)
 
     # create datasets
-    with log.subproc('Creating datasets'):
+    with log.subproc("Creating datasets"):
         dataset_dir = params_in.dataset_dir
-        train_set = MNISTDataset(dataset_dir / 'train.hdf5')
-        test_set = MNISTDataset(dataset_dir / 'test.hdf5')
-        log.message(f'Dataset directory: {dataset_dir}')
+        train_set = MNISTDataset(dataset_dir / "train.hdf5")
+        test_set = MNISTDataset(dataset_dir / "test.hdf5")
+        log.message(f"Dataset directory: {dataset_dir}")
 
     # create model
-    with log.subproc('Creating CNN model'):
+    with log.subproc("Creating CNN model"):
         model = create_model_mnist()
 
     # data
-    console.begin('Creating data loader')
+    console.begin("Creating data loader")
     dataset_dir = params_in.dataset_dir
-    train_dataloader = DataLoader(train_set, batch_size, shuffle=True)
-    test_dataloader = DataLoader(test_set, batch_size, shuffle=True)
-    console.ended('Creating data loader')
+    train_dataloader = DataLoader(
+        train_set, batch_size, shuffle=True, num_workers=0, pin_memory=True
+    )
+    test_dataloader = DataLoader(
+        test_set, batch_size, shuffle=False, num_workers=0, pin_memory=True
+    )
+    console.ended("Creating data loader")
 
-    # Training 
-    log.begin('Training CNN model')
-    with log.subproc('Running model.fit()'):
-        params_out.system.stamp_event('start train()')
-        history = train()
+    # Training
+    log.begin("Training CNN model")
+    with log.subproc("Running model.fit()"):
+        params_out.system.stamp_event("start train()")
+        optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+        for epoch in range(args["epochs"]):
+            train(model, train_dataloader, optimizer)
+            predict(model, test_dataloader)
+            log.message(f"epoch: {epoch}")
 
     # save model
-    with log.subproc('Saving the model'):
-        model_file = params_in.output_dir / 'mnist_torch_model.h5'
-        model.save(model_file)
-        log.message(f'Saved to: {model_file}')
+    with log.subproc("Saving the model"):
+        model_file = params_in.output_dir / "mnist_torch_model.h5"
+        torch.save(model.state_dict(), model_file)
+        log.message(f"Saved to: {model_file}")
 
-    # save history
-    with log.subproc('Saving training history'):
-        history_file = params_in.output_dir / 'training_history.yml'
-        with open(history_file, 'w') as handle:
-            yaml.dump(history.history, handle)
-        log.message(f'Saved to: {history_file}')
-    log.ended('Training CNN model')
+    log.ended("Training CNN model")
 
     # top-level process
-    console.ended('Running benchmark mnist_torch on training mode')
-
-
-
+    console.ended("Running benchmark mnist_torch on training mode")
--- sciml_bench/benchmarks/examples/mnist_torch/mnist_torch_misc.py
+++ sciml_bench/benchmarks/examples/mnist_torch/mnist_torch_misc.py
@@ -6,32 +6,29 @@
 # SciML-Bench
 # Copyright © 2021 Scientific Machine Learning Research Group
 # Scientific Computing Department, Rutherford Appleton Laboratory
-# Science and Technology Facilities Council, UK. 
+# Science and Technology Facilities Council, UK.
 # All rights reserved.
 
 """
 Various routes to be used by the mnist_torch benchmark
 """
 
-from pathlib import Path
-import yaml
 import torch
 import torch.utils.data as Data
-import torch.multiprocessing as mp
 import torch.nn as nn
 import torch.nn.functional as F
 import h5py
-import numpy as np
+
 
 class MNISTDataset(Data.Dataset):
-    """ mnist dataset """
+    """mnist dataset"""
 
     def __init__(self, file_path):
         super(MNISTDataset, self).__init__()
         # read whole
-        with h5py.File(file_path, 'r') as h5:
-            self.images = torch.tensor(h5['image'][:], dtype=torch.float32)
-            self.labels = torch.tensor(h5['label'][:], dtype=torch.long)
+        with h5py.File(file_path, "r") as h5:
+            self.images = torch.tensor(h5["image"][:], dtype=torch.float32)
+            self.labels = torch.tensor(h5["label"][:], dtype=torch.long)
         # expand channel dim and normalize
         self.images = torch.unsqueeze(self.images, 1) / 255
 
@@ -42,12 +39,11 @@ class MNISTDataset(Data.Dataset):
         return self.images.size(0)
 
 
-
 class MNISTNet(nn.Module):
-    """ Define a CNN """
+    """Define a CNN"""
 
     def __init__(self, n_filters, n_units_hidden):
-        """ Initialize network with some hyperparameters """
+        """Initialize network with some hyperparameters"""
         super(MNISTNet, self).__init__()
         self.conv = nn.Conv2d(1, n_filters, kernel_size=3)
         self.pool = nn.MaxPool2d(2)
@@ -59,5 +55,3 @@ class MNISTNet(nn.Module):
         x = F.relu(self.fc1(x.view(x.size(0), -1)))
         x = F.softmax(self.fc2(x), dim=1)
         return x
-
-
